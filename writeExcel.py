4b93a45cbfb71c60310cb2783e33f59a752d2fe1
plectrum0224 <plectrum@outlook.com>
2016-03-16 13:31:21 +0800

    add write list data function

--- writeExcel.py
+++ writeExcel.py
@@ -35,17 +35,33 @@ import openpyxl
 # wb.remove_sheet(wb.get_sheet_by_name('Sheet1'))
 # print(wb.get_sheet_names())
 
-wb = openpyxl.load_workbook('produceSales.xlsx')
-sheet = wb.get_sheet_by_name('Sheet')
-PRICE_UPDATES = {
-    'Garlic': 3.07,
-    'Celery': 1.19,
-    'Lemon':1.27
-}
+# wb = openpyxl.load_workbook('produceSales.xlsx')
+# sheet = wb.get_sheet_by_name('Sheet')
+# PRICE_UPDATES = {
+#     'Garlic': 3.07,
+#     'Celery': 1.19,
+#     'Lemon':1.27
+# }
+#
+# for rowNum in range(2, sheet.get_highest_row()):
+#     produceName = sheet.cell(row=rowNum, column=1).value
+#     if produceName in PRICE_UPDATES:
+#         sheet.cell(row=rowNum, column=2).value = PRICE_UPDATES[produceName]
+#
+# wb.save('updatedProduceSales.xlsx')
 
-for rowNum in range(2, sheet.get_highest_row()):
-    produceName = sheet.cell(row=rowNum, column=1).value
-    if produceName in PRICE_UPDATES:
-        sheet.cell(row=rowNum, column=2).value = PRICE_UPDATES[produceName]
 
-wb.save('updatedProduceSales.xlsx')
+listNum = [1, 2, 3, 100, 22,3,33,2,2,1,23,4,4,5,5]
+def recordList(listType, i):
+    countNum = 0
+    wb = openpyxl.Workbook()
+    wb.create_sheet(index=0, title='Signals')
+    sheet = wb.get_sheet_by_name('Signals')
+    for columnNum in range(1,i):
+        for rowNum in range(1, len(listNum)+1):
+            sheet.cell(row=rowNum, column=columnNum).value = listNum[rowNum-1]
+        countNum += 1
+    if countNum == (i-1):
+        wb.save('SignalExcel.xlsx')
+
+recordList(listNum, 10)
